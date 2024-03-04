efef6b77020584d5db71aad3efcf1d7805fe9ac3
wyf-cyber <wyf17326280236@gmail.com>
2024-03-04 22:19:29 +0800

    first homework

---
+++ assignment2/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Homework2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.num1 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.author = new System.Windows.Forms.Label();
+            this.op = new System.Windows.Forms.Label();
+            this.res = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(565, 546);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(479, 402);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(74, 36);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // num1
+            // 
+            this.num1.AutoSize = true;
+            this.num1.Font = new System.Drawing.Font("宋体", 14F);
+            this.num1.Location = new System.Drawing.Point(234, 332);
+            this.num1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(110, 28);
+            this.num1.TabIndex = 2;
+            this.num1.Text = "number1";
+            this.num1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // num2
+            // 
+            this.num2.AutoSize = true;
+            this.num2.Font = new System.Drawing.Font("宋体", 14F);
+            this.num2.Location = new System.Drawing.Point(672, 332);
+            this.num2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(110, 28);
+            this.num2.TabIndex = 3;
+            this.num2.Text = "number2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 399);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 39);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(666, 399);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 39);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14F);
+            this.label3.Location = new System.Drawing.Point(847, 405);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "=";
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(537, 49);
+            this.title.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(191, 51);
+            this.title.TabIndex = 7;
+            this.title.Text = "Calculator";
+            this.title.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // author
+            // 
+            this.author.AutoSize = true;
+            this.author.Font = new System.Drawing.Font("Consolas", 12F);
+            this.author.Location = new System.Drawing.Point(533, 144);
+            this.author.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.author.Name = "author";
+            this.author.Size = new System.Drawing.Size(194, 28);
+            this.author.TabIndex = 8;
+            this.author.Text = "Author:wyf-088";
+            // 
+            // op
+            // 
+            this.op.AutoSize = true;
+            this.op.Font = new System.Drawing.Font("宋体", 14F);
+            this.op.Location = new System.Drawing.Point(447, 332);
+            this.op.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.op.Name = "op";
+            this.op.Size = new System.Drawing.Size(138, 28);
+            this.op.TabIndex = 9;
+            this.op.Text = "operators";
+            this.op.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // res
+            // 
+            this.res.AutoSize = true;
+            this.res.Location = new System.Drawing.Point(928, 402);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(26, 28);
+            this.res.TabIndex = 11;
+            this.res.Text = "0";
+            this.res.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 700);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.op);
+            this.Controls.Add(this.author);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("宋体", 14F);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.Label num2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label author;
+        private System.Windows.Forms.Label op;
+        private System.Windows.Forms.Label res;
+    }
+}
+
